e7ce5d1a58c9012629c93a63eff27adb776c4b93
wanghao <martinwanghao@qq.com>
2019-03-01 14:29:48 +0800

    can run in VSCode

--- ATM.java
+++ ATM.java
@@ -1,8 +1,15 @@
+import java.util.Map;
+
 public class ATM {
     private static int cash5;
     private static int cash10;
     private static int cash20;
     private static int cash50;
     
-    Account[] accounts;
+    Map<String, Customer> CustomerMap;
+    Map<String, Manager> ManagerMap;
+
+    public static void main(String args[]) {
+        System.out.println("Hello Martin");
+    }
 }
